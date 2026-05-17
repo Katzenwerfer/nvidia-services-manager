768b5b65f1a2f77633ec44a0b54d0a7226a5364d
Katzenwerfer <53267697+Katzenwerfer@users.noreply.github.com>
2026-05-17 00:39:36 -0600

    Handle multiple policies

--- src/1_setup/Enable-ProcessTracking.ps1
+++ src/1_setup/Enable-ProcessTracking.ps1
@@ -1,22 +1,27 @@
 $ErrorActionPreference = 'SilentlyContinue'
 
-$SubcategoryName = 'Process Termination'
+$SubcategoryNames = @(
+    'Process Creation',
+    'Process Termination'
+)
 
-$Subcategory = & auditpol.exe /get /subcategory:"$SubcategoryName" /r | ConvertFrom-Csv
-$Status = $Subcategory | Select-Object -ExpandProperty 'Inclusion Setting'
+foreach ($SubcategoryName in $SubcategoryNames) {
+    $Subcategory = & auditpol.exe /get /subcategory:"$SubcategoryName" /r | ConvertFrom-Csv
+    $Status = $Subcategory | Select-Object -ExpandProperty 'Inclusion Setting'
 
-if ($Status -notlike '*Success*') {
-    Write-Host -Object "Audit policy '$SubcategoryName' is currently set to $Status." -ForegroundColor 'Yellow'
-    Write-Host -Object "Enabling 'Success' recording..." -ForegroundColor 'Cyan'
+    if ($Status -notlike '*Success*') {
+        Write-Host -Object "Audit policy '$SubcategoryName' is currently set to $Status." -ForegroundColor 'Yellow'
+        Write-Host -Object "Enabling 'Success' recording..." -ForegroundColor 'Cyan'
 
-    & auditpol.exe /set /subcategory:"$SubcategoryName" /success:enable | Out-Null
+        & auditpol.exe /set /subcategory:"$SubcategoryName" /success:enable | Out-Null
 
-    if ($LASTEXITCODE -ne 0) {
-        Write-Error -Message "Failed to configure policy for '$SubcategoryName'. Make sure you are running as Administrator." -ErrorAction 'Stop'
+        if ($LASTEXITCODE -ne 0) {
+            Write-Error -Message "Failed to configure policy for '$SubcategoryName'. Make sure you are running as Administrator." -ErrorAction 'Stop'
+        }
+
+        Write-Host -Object "Successfully updated '$SubcategoryName' to register 'Success' events." -ForegroundColor 'Green'
     }
-
-    Write-Host -Object "Successfully updated '$SubcategoryName' to register 'Success' events." -ForegroundColor 'Green'
-}
-else {
-    Write-Host -Object "Audit policy '$SubcategoryName' is already set to $Status. No action taken." -ForegroundColor 'Green'
+    else {
+        Write-Host -Object "Audit policy '$SubcategoryName' is already set to $Status. No action taken." -ForegroundColor 'Green'
+    }
 }
