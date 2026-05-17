4f9f7d0fb40b04970373ef10411e77fda63c71c1
Katzenwerfer <53267697+Katzenwerfer@users.noreply.github.com>
2026-05-17 13:35:37 -0600

    Fix path check

--- src/5_heal/Repair-NvCplSpawner.ps1
+++ src/5_heal/Repair-NvCplSpawner.ps1
@@ -7,7 +7,7 @@ $ScheduledTask = Get-ScheduledTask -TaskName $TaskName -TaskPath $TaskPath
 if ($ScheduledTask) {
     $ScheduledTask.Triggers.Subscription -match "Data='(.+)'"
     $TargetProcess = Resolve-Path -Path 'C:\Program Files\WindowsApps\NVIDIACorp.NVIDIAControlPanel_*\nvcplui.exe'
-    if ($Matches[1] -eq $TargetProcess) {
+    if ($Matches[1] -ne $TargetProcess) {
         $ScheduledTask | Unregister-ScheduledTask -Confirm:$false
         & "$PSScriptRoot\..\3_spawn\New-NvCplSpawner.ps1"
     }
