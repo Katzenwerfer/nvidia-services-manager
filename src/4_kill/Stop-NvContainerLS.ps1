2cc8b1299d00e39e8f114f6c14dd50bce585c4e3
Katzenwerfer <53267697+Katzenwerfer@users.noreply.github.com>
2026-05-17 02:48:28 -0600

    Enforce services startup type on every exit

--- src/4_kill/Stop-NvContainerLS.ps1
+++ src/4_kill/Stop-NvContainerLS.ps1
@@ -4,4 +4,5 @@ Start-Sleep -Seconds 3
 
 if (-not [bool](Get-Process -Name 'NVIDIA App' -ErrorAction 'SilentlyContinue')) {
     Stop-Service -Force -Name 'NvContainerLocalSystem'
+    & "$PSScriptRoot\..\2_disable\Set-NvContainerServices.ps1"
 }
