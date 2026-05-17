d8a7530b780348085c2517f903f4b01f10b80bad
Katzenwerfer <53267697+Katzenwerfer@users.noreply.github.com>
2026-05-17 01:10:22 -0600

    Reimplement error handling

--- src/2_disable/Set-NvContainerServices.ps1
+++ src/2_disable/Set-NvContainerServices.ps1
@@ -8,13 +8,21 @@ $ServiceNames = @(
 foreach ($ServiceName in $ServiceNames) {
     $Service = Get-Service -Name $ServiceName
 
+    if (-not $Service) {
+        Write-Error -Message "Service '$ServiceName' was not found on this system." -ErrorAction 'Stop'
+    }
+
     if ($Service.StartType -ne 'Manual') {
         Write-Host -Object "'$ServiceName' start type is currently set to '$($Service.StartType)'." -ForegroundColor 'Yellow'
         Write-Host -Object "Changing start type to 'Manual'..." -ForegroundColor 'Cyan'
 
-        Set-Service -Name $ServiceName -StartupType 'Manual' -ErrorAction 'Stop'
-
-        Write-Host -Object "Successfully changed start type to 'Manual'." -ForegroundColor 'Green'
+        try {
+            Set-Service -Name $ServiceName -StartupType 'Manual' -ErrorAction 'Stop'
+            Write-Host -Object "Successfully changed start type to 'Manual'." -ForegroundColor 'Green'
+        }
+        catch {
+            Write-Error 'Failed to update the service. Make sure you are running as Administrator.' -ErrorAction 'Stop'
+        }
     }
     else {
         Write-Host -Object "Service '$ServiceName' start type is currently  set to 'Manual'. No change required." -ForegroundColor 'Green'
