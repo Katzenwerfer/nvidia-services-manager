01029a201ec84e11cc90ce1f7be0f81b28739297
Katzenwerfer <53267697+Katzenwerfer@users.noreply.github.com>
2026-05-17 01:26:37 -0600

    Modify for task handling

--- src/2_disable/Disable-NvAppTask.ps1
+++ src/2_disable/Disable-NvAppTask.ps1
@@ -1,31 +1,26 @@
 $ErrorActionPreference = 'SilentlyContinue'
 
-$ServiceNames = @(
-    'NvContainerLocalSystem',
-    'NVDisplay.ContainerLocalSystem'
-)
+$TaskNameWildcard = 'NVIDIA App*'
 
-$TargetStartType = 'Manual'
+$TargetState = 'Disabled'
 
-foreach ($ServiceName in $ServiceNames) {
-    $Service = Get-Service -Name $ServiceName
+$Task = Get-ScheduledTask -TaskName $TaskNameWildcard
 
-    if ($null -eq $Service) {
-        Write-Error -Message "Service '$ServiceName' was not found on this system." -ErrorAction 'Stop'
+if ($null -eq $Task) {
+    Write-Error -Message 'Scheduled task could not found on this system.' -ErrorAction 'Stop'
+}
+
+if ($Task.State -ne $TargetState) {
+    Write-Host -Object "Scheduled task is currently set to '$($Task.State)'." -ForegroundColor 'Yellow'
+    Write-Host -Object "Changing state to '$TargetState'..." -ForegroundColor 'Cyan'
+    try {
+        Disable-ScheduledTask -TaskName $Task.TaskName -ErrorAction 'Stop' | Out-Null
+        Write-Host -Object "Successfully changed state to '$TargetState'." -ForegroundColor 'Green'
     }
-
-    if ($Service.StartType -ne $TargetStartType) {
-        Write-Host -Object "'$ServiceName' start type is currently set to '$($Service.StartType)'." -ForegroundColor 'Yellow'
-        Write-Host -Object "Changing start type to '$TargetStartType'..." -ForegroundColor 'Cyan'
-        try {
-            Set-Service -Name $ServiceName -StartupType $TargetStartType -ErrorAction 'Stop'
-            Write-Host -Object "Successfully changed start type to '$TargetStartType'." -ForegroundColor 'Green'
-        }
-        catch {
-            Write-Error "Failed to update the service. Make sure you are running as Administrator." -ErrorAction 'Stop'
-        }
+    catch {
+        Write-Error -Message "Failed to change scheduled task state. Make sure you are running as Administrator." -ErrorAction 'Stop'
     }
-    else {
-        Write-Host -Object "Service '$ServiceName' start type is currently  set to '$TargetStartType'. No change required." -ForegroundColor 'Green'
-    }
+}
+else {
+    Write-Host -Object "Scheduled task state is currently set to '$TargetState'. No action taken." -ForegroundColor 'Green'
 }
