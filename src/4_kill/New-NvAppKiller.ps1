ea15032d6f071d5c17ff92e0658caf19ed456005
Katzenwerfer <53267697+Katzenwerfer@users.noreply.github.com>
2026-05-17 01:32:25 -0600

    Add New-NvAppKiller.ps1

---
+++ src/4_kill/New-NvAppKiller.ps1
@@ -0,0 +1,56 @@
+$ErrorActionPreference = 'SilentlyContinue'
+
+$TaskName = 'NvContainerLocalSystem Killer (NVIDIA App.exe)'
+$TaskPath = '\Custom\'
+
+# --------------------------------
+# === Scheduled Task Principal ===
+# --------------------------------
+
+$UserId = "$env:USERDOMAIN\$env:USERNAME"
+
+$ScheduledTaskPrincipal = New-ScheduledTaskPrincipal -UserId $UserId -LogonType 'S4U'
+
+# -----------------------------
+# === Scheduled Task Action ===
+# -----------------------------
+
+$ActionProcess = '"C:\Program Files\PowerShell\7\pwsh.exe"'
+$ActionScript = "$PSScriptRoot\Stop-NvContainerLS.ps1"
+$ActionParameters = "-ExecutionPolicy Bypass -NoLogo -NonInteractive -NoProfile -WindowStyle Hidden -File `"$ActionScript`""
+
+$ScheduledTaskAction = New-ScheduledTaskAction -Execute $ActionProcess -Argument $ActionParameters
+
+# -------------------------------
+# === Scheduled Task Settings ===
+# -------------------------------
+
+$ScheduledTaskSettings = New-ScheduledTaskSettingsSet -AllowStartIfOnBatteries -StartWhenAvailable -DontStopIfGoingOnBatteries -MultipleInstances 'IgnoreNew'
+
+# ------------------------------
+# === Scheduled Task Trigger ===
+# ------------------------------
+
+$TargetProcess = 'C:\Program Files\NVIDIA Corporation\NVIDIA App\CEF\NVIDIA App.exe'
+$TargetQuery = @"
+<QueryList>
+    <Query Id="0" Path="Security">
+        <Select Path="Security">
+            *[System[Provider[@Name='Microsoft-Windows-Security-Auditing'] and
+            (EventID=4689) and
+            (Task=13313)]] and
+            *[EventData[Data[@Name='ProcessName'] and
+            (Data='$TargetProcess')]]
+        </Select>
+    </Query>
+</QueryList>
+"@
+
+$ScheduledTaskTrigger = Get-CimClass -ClassName 'MSFT_TaskEventTrigger' -Namespace 'Root/Microsoft/Windows/TaskScheduler' | New-CimInstance -ClientOnly
+$ScheduledTaskTrigger.Subscription = $TargetQuery
+
+# ----------------------
+# === Scheduled Task ===
+# ----------------------
+
+New-ScheduledTask -Action $ScheduledTaskAction -Principal $ScheduledTaskPrincipal -Settings $ScheduledTaskSettings -Trigger $ScheduledTaskTrigger | Register-ScheduledTask -TaskName $TaskName -TaskPath $TaskPath
