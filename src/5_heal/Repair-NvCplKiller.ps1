2336d5fcee254ecb6c137ebe403f18861bd9f556
Katzenwerfer <53267697+Katzenwerfer@users.noreply.github.com>
2026-05-17 01:51:19 -0600

    Add Repair-NvCplKiller.ps1

---
+++ src/5_heal/Repair-NvCplKiller.ps1
@@ -0,0 +1,18 @@
+$ErrorActionPreference = 'SilentlyContinue'
+
+$TaskName = 'NvDisplayContainerLocalSystem Killer (nvcplui.exe)'
+$TaskPath = '\Custom\'
+
+$ScheduledTask = Get-ScheduledTask -TaskName $TaskName -TaskPath $TaskPath
+
+if ($ScheduledTask) {
+    $ScheduledTask.Triggers.Subscription -match "Data='(.+)'" | Out-Null
+    $TargetProcess = Resolve-Path -Path 'C:\Program Files\WindowsApps\NVIDIACorp.NVIDIAControlPanel_*\nvcplui.exe'
+    if ($Matches[1] -ne $TargetProcess) {
+        $ScheduledTask | Unregister-ScheduledTask -Confirm:$false
+        & "$PSScriptRoot\..\4_kill\New-NvCplKiller.ps1"
+    }
+}
+else {
+    & "$PSScriptRoot\..\4_kill\New-NvCplKiller.ps1"
+}
