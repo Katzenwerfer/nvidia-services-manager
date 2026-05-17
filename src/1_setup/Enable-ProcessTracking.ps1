ba8cb95ec0c5a4df27dfda71dbeb6e56a2f6bc24
Katzenwerfer <53267697+Katzenwerfer@users.noreply.github.com>
2026-05-17 00:38:20 -0600

    Add verbose messages

--- src/1_setup/Enable-ProcessTracking.ps1
+++ src/1_setup/Enable-ProcessTracking.ps1
@@ -2,4 +2,17 @@ $ErrorActionPreference = 'SilentlyContinue'
 
 $SubcategoryName = 'Process Termination'
 
-& auditpol.exe /set /subcategory:"$SubcategoryName" /success:enable | Out-Null
+$Subcategory = & auditpol.exe /get /subcategory:"$SubcategoryName" /r | ConvertFrom-Csv
+$Status = $Subcategory | Select-Object -ExpandProperty 'Inclusion Setting'
+
+if ($Status -notlike '*Success*') {
+    Write-Host -Object "Audit policy '$SubcategoryName' is currently set to $Status." -ForegroundColor 'Yellow'
+    Write-Host -Object "Enabling 'Success' recording..." -ForegroundColor 'Cyan'
+
+    & auditpol.exe /set /subcategory:"$SubcategoryName" /success:enable | Out-Null
+
+    Write-Host -Object "Successfully updated '$SubcategoryName' to register 'Success' events." -ForegroundColor 'Green'
+}
+else {
+    Write-Host -Object "Audit policy '$SubcategoryName' is already set to $Status. No action taken." -ForegroundColor 'Green'
+}
