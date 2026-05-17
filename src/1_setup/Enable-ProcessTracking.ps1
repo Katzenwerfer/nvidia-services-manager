e09365c375af0efb5613273b0e96fff0aa6d96af
Katzenwerfer <53267697+Katzenwerfer@users.noreply.github.com>
2026-05-17 01:42:57 -0600

    Include error handling

--- src/1_setup/Enable-ProcessTracking.ps1
+++ src/1_setup/Enable-ProcessTracking.ps1
@@ -9,6 +9,9 @@ if ($Status -notlike '*Success*') {
     Write-Host -Object "Audit policy '$SubcategoryName' is currently set to $Status." -ForegroundColor 'Yellow'
     Write-Host -Object "Enabling 'Success' recording..." -ForegroundColor 'Cyan'
     & auditpol.exe /set /subcategory:"$SubcategoryName" /success:enable | Out-Null
+    if ($LASTEXITCODE -ne 0) {
+        Write-Error -Message "Failed to configure policy for '$SubcategoryName'. Make sure you are running as Administrator." -ErrorAction 'Stop'
+    }
     Write-Host -Object "Successfully updated '$SubcategoryName' to register 'Success' events." -ForegroundColor 'Green'
 }
 else {
