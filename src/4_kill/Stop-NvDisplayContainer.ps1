9a3a7d0557872100905675d29e7525404893b39d
Katzenwerfer <53267697+Katzenwerfer@users.noreply.github.com>
2026-05-17 18:16:10 -0600

    Fix wrong target process

--- src/4_kill/Stop-NvDisplayContainer.ps1
+++ src/4_kill/Stop-NvDisplayContainer.ps1
@@ -2,7 +2,7 @@ $ErrorActionPreference = 'SilentlyContinue'
 
 Start-Sleep -Seconds 3
 
-if (-not [bool](Get-Process -Name 'NVIDIA App' -ErrorAction 'SilentlyContinue')) {
+if (-not [bool](Get-Process -Name 'nvcplui' -ErrorAction 'SilentlyContinue')) {
     Stop-Service -Force -Name 'NVDisplay.ContainerLocalSystem'
     & "$PSScriptRoot\..\2_disable\Set-NvContainerServices.ps1"
 }
