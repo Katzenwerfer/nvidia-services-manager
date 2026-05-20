388b2e56c1f1608d3ccbf364fa1cb44e536b9616
Katzenwerfer <53267697+Katzenwerfer@users.noreply.github.com>
2026-05-20 11:59:31 -0600

    Target new scripts

--- src/4_kill/Stop-NvDisplayContainer.ps1
+++ src/4_kill/Stop-NvDisplayContainer.ps1
@@ -4,5 +4,4 @@ Start-Sleep -Seconds 3
 
 if (-not [bool](Get-Process -Name 'nvcplui' -ErrorAction 'SilentlyContinue')) {
     Stop-Service -Force -Name 'NVDisplay.ContainerLocalSystem'
-    & "$PSScriptRoot\..\2_disable\Set-NvContainerServices.ps1"
 }
