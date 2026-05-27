f7fe4d2275373563bc41bdf5f7e4a8d8974d54e3
Katzenwerfer <53267697+Katzenwerfer@users.noreply.github.com>
2026-05-26 21:16:19 -0600

    Ensure the update task is disabled

--- src/4_kill/Stop-NvContainerLS.ps1
+++ src/4_kill/Stop-NvContainerLS.ps1
@@ -4,5 +4,6 @@ Start-Sleep -Seconds 2
 
 if (-not [bool](Get-Process -Name 'NVIDIA App' -ErrorAction 'SilentlyContinue')) {
     Stop-Service -Force -Name 'NvContainerLocalSystem'
+    & "$PSScriptRoot\..\2_disable\Disable-NvAppTask.ps1"
     & "$PSScriptRoot\..\2_disable\Set-NvContainerLS.ps1"
 }
