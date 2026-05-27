d1a0de06479ba9bea3fa185b86bf3b548ad7719a
Katzenwerfer <53267697+Katzenwerfer@users.noreply.github.com>
2026-05-26 21:30:24 -0600

    Add setup.ps1

---
+++ src/setup.ps1
@@ -0,0 +1,20 @@
+# Part 1
+
+& "$PSScriptRoot\1_setup\Enable-ProcessTracking.ps1"
+
+# Part 2
+
+& "$PSScriptRoot\2_disable\Disable-NvAppTask.ps1"
+& "$PSScriptRoot\2_disable\Set-NvContainerLS.ps1"
+& "$PSScriptRoot\2_disable\Set-NvDisplayContainerLS.ps1"
+
+# Part 3
+
+& "$PSScriptRoot\3_spawn\New-NvCplSpawner.ps1"
+
+# Part 4
+
+& "$PSScriptRoot\4_kill\New-NvAppKiller.ps1"
+& "$PSScriptRoot\4_kill\New-NvCplKiller.ps1"
+& "$PSScriptRoot\4_kill\Stop-NvContainerLS.ps1"
+& "$PSScriptRoot\4_kill\Stop-NvDisplayContainer.ps1"
