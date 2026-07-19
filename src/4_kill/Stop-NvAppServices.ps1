942503fd25e7e36b801afb15ddf10d8ea38ddf78
Katzenwerfer <53267697+Katzenwerfer@users.noreply.github.com>
2026-07-19 05:07:04 -0600

    Conditionally start/stop services

--- src/4_kill/Stop-NvAppServices.ps1
+++ src/4_kill/Stop-NvAppServices.ps1
@@ -5,8 +5,18 @@ Start-Sleep -Seconds 2
 $Process = Get-Process -Name 'NVIDIA App'
 
 if (-not $Process) {
-    Stop-Service -Force -Name 'NVDisplay.ContainerLocalSystem'
-    Stop-Service -Force -Name 'NvContainerLocalSystem'
+    $NVDisplayContainerLS = Get-Service -Name 'NVDisplay.ContainerLocalSystem'
+
+    if ($NVDisplayContainerLS.Status -eq 'Running') {
+        $NvDisplayContainerLS | Stop-Service -Force
+    }
+
+    $NvContainerLS = Get-Service -Name 'NvContainerLocalSystem'
+
+    if ($NvContainerLS.Status -eq 'Running') {
+        $NvContainerLS | Stop-Service -Force
+    }
+
     & "$PSScriptRoot\..\2_disable\Disable-NvAppTask.ps1"
     & "$PSScriptRoot\..\2_disable\Set-NvContainerLS.ps1"
 }
