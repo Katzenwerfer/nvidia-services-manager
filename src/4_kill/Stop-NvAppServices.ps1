b80d883ece8ca3003cc9296dad20c2728fb48d39
Katzenwerfer <53267697+Katzenwerfer@users.noreply.github.com>
2026-09-16 03:04:37 -0600

    Skip NvApp services if overlay is enabled

--- src/4_kill/Stop-NvAppServices.ps1
+++ src/4_kill/Stop-NvAppServices.ps1
@@ -3,8 +3,9 @@ $ErrorActionPreference = 'SilentlyContinue'
 Start-Sleep -Seconds 2
 
 $Process = Get-Process -Name 'NVIDIA App'
+$Overlay = Get-Process -Name 'NVIDIA Overlay'
 
-if (-not $Process) {
+if (-not $Process -and -not $Overlay) {
     $NVDisplayContainerLS = Get-Service -Name 'NVDisplay.ContainerLocalSystem'
 
     if ($NVDisplayContainerLS.Status -eq 'Running') {
@@ -16,7 +17,7 @@ if (-not $Process) {
     if ($NvContainerLS.Status -eq 'Running') {
         $NvContainerLS | Stop-Service -Force
     }
-
-    & "$PSScriptRoot\..\2_disable\Disable-NvAppTask.ps1"
-    & "$PSScriptRoot\..\2_disable\Set-NvContainerLS.ps1"
 }
+
+& "$PSScriptRoot\..\2_disable\Disable-NvAppTask.ps1"
+& "$PSScriptRoot\..\2_disable\Set-NvContainerLS.ps1"
