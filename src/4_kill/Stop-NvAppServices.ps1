17657ef95441208edb0f83e7e602d23d67d505ea
Katzenwerfer <53267697+Katzenwerfer@users.noreply.github.com>
2026-09-16 03:20:02 -0600

    Don't handle NvDisplayContainer in NvApp killer I'm not sure the NVIDIA App uses the service at all

--- src/4_kill/Stop-NvAppServices.ps1
+++ src/4_kill/Stop-NvAppServices.ps1
@@ -6,12 +6,6 @@ $Process = Get-Process -Name 'NVIDIA App'
 $Overlay = Get-Process -Name 'NVIDIA Overlay'
 
 if (-not $Process -and -not $Overlay) {
-    $NVDisplayContainerLS = Get-Service -Name 'NVDisplay.ContainerLocalSystem'
-
-    if ($NVDisplayContainerLS.Status -eq 'Running') {
-        $NvDisplayContainerLS | Stop-Service -Force
-    }
-
     $NvContainerLS = Get-Service -Name 'NvContainerLocalSystem'
 
     if ($NvContainerLS.Status -eq 'Running') {
